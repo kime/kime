02c1d7dbe506203b4e147f7abf8741895f355586
Achyudh Ram <achyudhk@gmail.com>
2019-04-22 06:29:51 -0400

    Fix auth routes to make them API v1 compliant

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,31 +1,11 @@
 from flask import Blueprint, session, url_for, g, jsonify
 from flask import redirect, request
 
-from app.extensions import db, auth
+from app.extensions import auth
 from app.models.user import User
 from app.responses import bad_request
 
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-@blueprint.route('/signup', methods=('GET', 'POST'))
-def signup():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        return bad_request()
-    if User.query.filter_by(username=username).first() is not None:
-        return bad_request('Username Exists')
-
-    user = User(username=username)
-    user.hash_password(password)
-    db.session.add(user)
-    db.session.commit()
-
-    return jsonify(
-        {'username': user.username,
-         'url': url_for('api.get_user', id=user.id, _external=True)
-         }), 201
 
 
 @auth.verify_password
@@ -46,28 +26,45 @@ def verify_password(auth_token, password):
             return False
 
     # Set Flask global user
+    session['user_id'] = user.id
     g.user = user
     return True
+
+
+@blueprint.before_app_request
+def get_current_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).one()
+
+
+@blueprint.route('/signup', methods=['POST'])
+def signup():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        return bad_request()
+    if User.query.filter_by(username=username).first() is not None:
+        # TODO: Remove redundant check for duplicate username
+        return bad_request('Username Exists')
+
+    user = User.add_user(username, password)
+
+    return jsonify({'username': user.username}), 201
+
+
+@blueprint.route('/login', methods=['POST', 'GET'])
+@auth.login_required
+def login():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'username': g.user.username,
+                    'token': token.decode('ascii'),
+                    'duration': 600})
 
 
 @blueprint.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('home.index'))
-
-
-@blueprint.route('/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token(600)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
-
-
-@blueprint.before_app_request
-def get_current_user():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = User.query.filter_by(id=user_id).first()
