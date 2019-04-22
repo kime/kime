7ec61a448cb9351b9430695e3d002639742b7435
Achyudh Ram <achyudhk@gmail.com>
2019-04-22 06:28:09 -0400

    Add Azure BlobStorage service

---
+++ app/services/storage/azure.py
@@ -0,0 +1,42 @@
+from azure.storage.blob import BlockBlobService
+
+from app import config
+from app.util.io import image_to_bytes, bytes_to_image
+
+
+def download_image(container, blob_name):
+    """
+    Downloads a blob as an array of bytes, and returns a
+    PIL image object initialized using those bytes
+    :param container: name of an existing storage container
+    :param blob_name: name of the blob containing the image
+    :return: a PIL Image object
+    """
+    account_name, access_key = config.azure_storage()
+    blob_service = BlockBlobService(account_name, access_key)
+    blob = blob_service.get_blob_to_bytes(container, blob_name)
+    return bytes_to_image(blob.content)
+
+
+def upload_image(image, container, blob_name):
+    """
+    Converts a PIL Image object into a bytestream and uploads it
+    to the given container in Azure Blob Storage
+    :param image: a PIL Image object
+    :param container: name of an existing storage container
+    :param blob_name: name of the blob to save the image as
+    :return: ETag and last modified properties for the Block Blob
+    """
+    account_name, access_key = config.azure_storage()
+    blob_service = BlockBlobService(account_name, access_key)
+    return blob_service.create_blob_from_bytes(container, blob_name, image_to_bytes(image))
+
+
+def get_blob_url(container, blob_name):
+    """
+
+    :param container:
+    :param blob_name:
+    :return:
+    """
+    return 'https://kimeimagestore.blob.core.windows.net/%s/%s' % (container, blob_name)
